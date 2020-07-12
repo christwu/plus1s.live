5ded55730b70b3a72baef57ac898f09386ae7925
infnan <38274826+infnan@users.noreply.github.com>
2020-07-12 22:42:55 +0800

    提升了使用Nodejs续秒的体验

--- stream.js
+++ stream.js
@@ -1,72 +1,144 @@
-const express = require("express");
-const fs = require("fs");
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const websocket = require('ws');
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SS' }),
+        winston.format.printf(info => `[${info.timestamp}] [${info.level}] - ${info.message}`)
+    ),
+    transports: [new winston.transports.Console()]
+});
 
-const MAX_FRAME = 360;
-let frames = new Array(MAX_FRAME);
+// count times
+let receivedTimes = 0;
 
-function plus1s(req, res, next) {
-    let curlMode=true;
-    let contentType = 'text/plain; charset=utf-8';
-    if (!req.header("User-Agent").includes("curl")) {
-        //res.redirect("https://github.com/HFO4/plus1s.live");
-        //return;
-        curlMode = false;
-        contentType = 'text/html; charset=utf-8';
+// Preparing frames...
+const picDir = process.env.PICDIR || 'pic';
+const page = fs.readFileSync('index.html').toString();
+const frames = fs.readdirSync(picDir)
+    .sort()
+    .filter(f => f.endsWith('.txt'))
+    .map(f => fs.readFileSync(path.join(picDir, f)).toString());
+
+if (frames.length === 0) {
+    logger.erorr('No pictures. Exit.');
+    process.exit(1);
+}
+
+const getIp = (req) => {
+    let ip = req.connection.remoteAddress;
+
+    if (req.headers['x-real-ip']) {
+        ip = req.headers['x-real-ip'];
+    } else if (req.headers['x-forwarded-for']) {
+        ip = req.headers['x-forwarded-for'].split(/,/)[0].trim();
     }
-    res.writeHead(200, {
-        'Content-Type': contentType,
-        'Transfer-Encoding': 'chunked',
-        'X-Content-Type-Options': 'nosniff'});
-    let i = 1;
-    let loop = 0;
-    let j = 0;
-    var io = setInterval(function () {
-        if (i == MAX_FRAME) {
-            i = 0;
-            loop++;
-        }
-        if (curlMode) {
-            write(res, frames[i]);
-        } else {
-            res.write(`<span id="ha${j}" style="white-space: pre-wrap; font-family: monospace;">`);
+
+    return ip;
+};
+
+// +1s...
+const collectTime = () => {
+    receivedTimes++;
+    if (receivedTimes % 3600 === 0) {
+        logger.info(`Received 1 hours. Now it's ${receivedTimes} seconds.`);
+    }
+};
+
+const plus1s = (req, res) => {
+    let userAgent = req.headers['user-agent'];
+    if (typeof userAgent !== 'string') {
+        userAgent = '';
+    }
+
+    if (userAgent.includes('curl')) {
+        // get ip
+        let ip = getIp(req);
+        logger.info(`Received 1 second from ${ip}`);
+
+        collectTime();
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Transfer-Encoding': 'chunked',
+            'X-Content-Type-Options': 'nosniff'
+        });
+
+        let i = 1;
+        let loop = 0;
+        const io = setInterval(function () {
             res.write(frames[i]);
-            res.write('</span>');
-            res.write(`<script type="text/javascript" id="s${j}">window.scrollTo(0,1000000000);try{document.getElementById("ha${j-1}").remove();}catch(e){}document.getElementById("s${j}").remove();</script>`);
-            j++;
-        }
-        i++;
-        if (loop >= 20) {
-            res.end("\nYou've waste too much time, we have to stop you.\n");
-            clearInterval(io);
-        }
-    }, 100);
-    if (next) {
-        next();
+            res.write('\033[2J\033[H');
+
+            i++;
+            if (i === frames.length) {
+                i = 0;
+                loop++;
+            }
+            if (loop === 20) {
+                res.end("\nYou've contributed too much time, so you're contributed too.\n");
+                logger.warn(`Oops! ${ip}'s life is contributed!`);
+                clearInterval(io);
+            }
+        }, 100);
+
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8',
+            'Transfer-Encoding': 'chunked',
+            'X-Content-Type-Options': 'nosniff'
+        });
+        res.end(page);
     }
-}
+};
 
-function write(res, str) {
-    res.write(str);
-    res.write("\033[2J\033[H");    
-}
-
-function prepare() {
-    console.log("Prepare frames...");
-    for (let i = 0; i < MAX_FRAME; i++) {
-        let buf = fs.readFileSync("pic/" + (i + 1).toString().padStart(3, '0') + ".txt");
-        if (buf) {
-            frames[i] = buf.toString();
-        }
+const server = http.createServer((req, res) => {
+    if (req.url === '/times') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8',
+        });
+        res.end(`${receivedTimes}`);
+    } else if (req.url.indexOf('favicon.ico') > -1) {
+        res.end();
+    } else {
+        plus1s(req, res);
     }
-    console.log("Frames are ready.");
-}
+});
 
-function main() {
-    prepare();
-    console.log("Ready to start server");
-    let app = express();
-    app.use('/', plus1s);
-    app.listen(1926).on('error', console.log);
-}
+const wss = new websocket.Server({
+    server: server
+});
 
-main();
+wss.on('connection', (ws, req) => {
+    let ip = getIp(req);
+    logger.info(`Received 1 second from ${ip}`);
+    collectTime();
+
+    ws.send(JSON.stringify({
+        length: frames.length
+    }));
+
+    ws.on('message', (message) => {
+        if (message === 'plus1s') {
+            for (let i = 0; i < frames.length; i++) {
+                ws.send(JSON.stringify({
+                    i: i,
+                    data: frames[i]
+                }));
+            }
+        } else if (message === 'nomoreseconds') {
+            const ip = getIp(req);
+            logger.warn(`Oops! ${ip}'s life is contributed!`);
+        }
+    });
+});
+
+const port = process.env.PORT || 1926;
+
+server.listen(port, () => {
+    logger.info(`http/ws server listening on 0.0.0.0:${port}`);
+}).on('error', logger.error);
+
